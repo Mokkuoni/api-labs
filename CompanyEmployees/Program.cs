43935291259f62d85ef57a09d5654650c8912203
makeevaanna353 <makeevaanna353@gmail.com>
2023-11-02 11:12:17 +0300

    search and sort

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,10 +1,13 @@
 using CompanyEmployees.ActionFilters;
 using CompanyEmployees.Controllers;
 using CompanyEmployees.Extensions;
+using Contracts;
+using Entities.DataTransferObjects;
 using FluentAssertions.Common;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using NLog;
+using Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,6 +37,7 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
     options.SuppressModelStateInvalidFilter = true;
     builder.Services.AddScoped<ValidationFilterAttribute>();
     builder.Services.AddScoped<ValidateCompanyExistsAttribute>();
+    builder.Services.AddScoped<IDataShaper<EmployeeDto>, DataShaper<EmployeeDto>>();
 });
 
 var nlogPath = Directory.GetCurrentDirectory() + "\\nlog.config";
